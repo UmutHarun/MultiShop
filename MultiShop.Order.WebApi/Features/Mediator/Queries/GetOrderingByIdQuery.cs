c5d571125320db94133b557a8259ec942e5cd1d5
UmutHarunJS <harun123ors@gmail.com>
2024-05-30 20:02:19 +0300

    ordering handlers added

---
+++ MultiShop.Order.WebApi/Features/Mediator/Queries/GetOrderingByIdQuery.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using MultiShop.Order.Application.Features.CQRS.Results.OrderDetailResults;
+using MultiShop.Order.Application.Features.Mediator.Results.OrderingResults;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.Mediator.Queries
+{
+    public class GetOrderingByIdQuery : IRequest<GetOrderingByIdQueryResult>
+    {
+        public GetOrderingByIdQuery(int id)
+        {
+            OrderingId = id;
+        }
+
+        public int OrderingId { get; set; }
+    }
+}
